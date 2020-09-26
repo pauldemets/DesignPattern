bcd275f8f097700c92d91df868449a1433394b82
Paul Demets <paul.demets@hotmail.com>
2020-09-26 17:55:22 +0200

    setup project

---
+++ DesingPatternProject.js
@@ -0,0 +1,109 @@
+/**
+ * 
+ * Au moins une Facory -- OK
+ * Au moins un Singletton
+ * Au moins un Constructor -- OK
+ * Au moins une Facade
+ * Au moins un Observe
+ * Au moins un décorateur
+ * 
+ */
+
+
+const photoFile = function (params) {
+    this.name = 'photo';
+    const { filename, size, type, pixelNumber } = params;
+    this.filename = filename;
+    this.size = size;
+    this.type = type;
+    this.pixelNumber = pixelNumber;
+};
+
+const videoFile = function (params) {
+    this.name = 'video';
+    const { filename, size, type, hd } = params;
+    this.filename = filename;
+    this.size = size;
+    this.type = type;
+    this.hd = hd;
+};
+
+const songFile = function (params) {
+    this.name = 'song';
+    const { filename, size, type, converted } = params;
+    this.filename = filename;
+    this.size = size;
+    this.type = type;
+    this.converted = converted;
+};
+
+const textFile = function (params) {
+    this.name = 'text';
+    const { filename, size, type, pixelNumber } = params;
+    this.filename = filename;
+    this.size = size;
+    this.type = type;
+    this.encrypted = encrypted;
+};
+
+
+const NumericFileFactory = function () { };
+
+NumericFileFactory.prototype.numericFileClass = Object;
+NumericFileFactory.prototype.createFile = function (type, params = {}) {
+    switch (type) {
+        case photoFile.name: this.numericFileClass = photoFile;
+            break;
+        case videoFile.name: this.numericFileClass = videoFile;
+            break;
+        case songFile.name: this.numericFileClass = songFile;
+            break;
+        case textFile.name: this.numericFileClass = textFile;
+            break;
+    }
+    return new this.numericFileClass(params);
+};
+
+
+
+const myNumericFileFactory = new NumericFileFactory();
+const Photo = myNumericFileFactory.createFile('photo', { filename: 'testFile', size: '512MB', type: 'JPEG', pixelNumber: '567k' });
+const Song = myNumericFileFactory.createFile('song', { filename: 'songTest', size: '1.02GB', type: 'MP3', converted: 'true' });
+const Text = myNumericFileFactory.createFile('text', { filename: 'textFile', size: '1.02GB', type: 'MP3', encrypted: 'true' });
+const Video = myNumericFileFactory.createFile('video', { filename: 'videoFile', size: '1.02GB', type: 'MP3', hd: 'false' });
+
+
+
+let readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '>'
+});
+
+readline.prompt();
+
+let commands = {
+
+    pwd: function () {
+        console.log('Tu as fait un pwd');
+        readline.prompt();
+    },
+    close: function () {
+        console.log('Closing script...');
+        readline.close();
+    },
+    photo: function() {
+        console.log('Affichage de la photo :', Photo);
+        readline.prompt();
+    }
+};
+
+readline.on('line', (input) => {
+    input = input.toLowerCase();
+    if (input in commands) {
+        commands[input]();
+    } else {
+        console.log('Sorry unknown command...');
+        readline.prompt();
+    }
+});
