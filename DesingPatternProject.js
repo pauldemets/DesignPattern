4da2da48bf08a3003d60617ec2e9d13a15a2e5c1
Paul Demets <paul.demets@hotmail.com>
2020-10-03 17:25:57 +0200

    Singleton + comment code

--- DesingPatternProject.js
+++ DesingPatternProject.js
@@ -1,11 +1,11 @@
 /**
  * 
  * Au moins une Facory -- OK
- * Au moins un Singletton
+ * Au moins un Singletton -- OK
  * Au moins un Constructor -- OK
  * Au moins une Facade
  * Au moins un Observe 
- * Au moins un décorateur OK
+ * Au moins un décorateur -- OK
  * 
  */
 
@@ -78,53 +78,96 @@ const fileDisplayInfo = function (numericFileClass) { //Decorateur = surcharge d
 };
 
 
-const listFiles = function () { //--> SINGLETON TODO, objets unique de la liste des files
-    this.myList = [];
-    this.index = -1;
-    this.counter = 0;
+/**
+ * --------SINGLETON--------
+ * Objet qui représente la liste de tous les fichier
+ * on va pouvoir effectuer différentes actions sur cette liste comme  : 
+ * 
+ * - ajouter un fichier dans la liste (addNewFile)
+ * - lire le fichier suivant/precedent (showNextFile/showPreviousFile)
+ * - voir combien de fichiers on a vu (getSize)
+ * - voir le nombre total de fichiers présents (getFilesCountSeen)
+ * 
+ */
+const listFiles = (function () {
 
-    this.addNewFile = function (file) {
-        this.myList.push(file);
-    }
+    let instance;
 
-    this.checkIfSeen= function(file){
-        if(!file.getSeen()){
-            this.counter ++;
+    const init = function () {
+        let myList = [];
+        let index = -1;
+        let counter = 0;
+
+        const addNewFile = function (file) {
+            myList.push(file);
+        }
+
+        const checkIfSeen = function (file) {
+            if (!file.getSeen()) {
+                counter++;
+            }
+        }
+
+        const getFilesCountSeen = function () {
+            return counter;
+        }
+
+        const getSize = function () {
+            return myList.length;
+        }
+
+        const showNextFile = function () {
+            index++;
+            if (index >= myList.length) {
+                index = 0;
+            }
+
+            checkIfSeen(myList[index]);
+
+            console.log(myList[index].getDescription());
+            myList[index].setSeen();
+        }
+        const showPreviousFile = function () {
+            index--;
+
+            if (index < 0) {
+                index = myList.length - 1;
+            }
+            checkIfSeen(myList[index]);
+
+            console.log(myList[index].getDescription());
+            myList[index].setSeen();
+        }
+
+        return {
+            addNewFile,
+            showNextFile,
+            showPreviousFile,
+            getSize,
+            getFilesCountSeen
         }
     }
 
-    this.getFilesCountSeen = function(){
-        return this.counter;
-    }
-
-    this.getSize = function(){
-        return this.myList.length;
-    }
-
-    this.showNextFile = function () {
-        this.index++;
-        if (this.index >= this.myList.length) {
-            this.index = 0;
+    return {
+        get: () => {
+            if (!instance) {
+                instance = init();
+            }
+            return instance;
         }
-
-        this.checkIfSeen(this.myList[this.index]);
-    
-        console.log(this.myList[this.index].getDescription());
-        this.myList[this.index].setSeen();
     }
-    this.showPreviousFile = function () {
-        this.index--;
+})();
 
-        if (this.index < 0) {
-            this.index = this.myList.length - 1;
-        }
-        this.checkIfSeen(this.myList[this.index]);
 
-        console.log(this.myList[this.index].getDescription());
-        this.myList[this.index].setSeen();
-    }
-}
-
+/**
+ * --------FACTORY--------
+ * Factory qui permet le création des différents fichiers : 
+ * 
+ * - photo
+ * - video
+ * - sons
+ * - text
+ */
 const NumericFileFactory = function () { };
 
 NumericFileFactory.prototype.numericFileClass = undefined;
@@ -146,73 +189,74 @@ NumericFileFactory.prototype.createFile = function (type, params = {}) {
 };
 
 
+
+/**
+ * --------INSTANCIATION--------
+ * Ici on va créer tous les objets que l'on doit créer (cf sujet projet)
+ */
 const myNumericFileFactory = new NumericFileFactory();
 const photo = myNumericFileFactory.createFile('photoFile', { filename: 'testFile', size: '512MB', type: 'JPEG', pixelNumber: '567k' });
 const song = myNumericFileFactory.createFile('songFile', { filename: 'songTest', size: '1.02GB', type: 'MP3', converted: 'true' });
 const text = myNumericFileFactory.createFile('textFile', { filename: 'textFile', size: '1.02GB', type: 'MP3', encrypted: 'true' });
 const video = myNumericFileFactory.createFile('videoFile', { filename: 'videoFile', size: '1.02GB', type: 'MP3', hd: 'false' });
 
+/**
+ * Après avoir créer les objects on créer les objets avec décorateur
+ * pour profiter de plus de méthodes/attributs
+ */
 const p1 = new fileDisplayInfo(photo);
 const s1 = new fileDisplayInfo(song);
 const t1 = new fileDisplayInfo(text);
 const v1 = new fileDisplayInfo(video);
 
+/**
+ * On ajoute les objets dans la liste
+ */
+listFiles.get().addNewFile(p1);
+listFiles.get().addNewFile(s1);
+listFiles.get().addNewFile(t1);
+listFiles.get().addNewFile(v1);
 
-const myFiles = new listFiles();
-myFiles.addNewFile(p1);
-myFiles.addNewFile(s1);
-myFiles.addNewFile(t1);
-myFiles.addNewFile(v1);
-
-
-
-
-
-// const commands = {
-//     pwd: function () {
-//         console.log('Tu as fait un pwd');
-//         readline.prompt();
-//     },
-//     exit: function () {
-//         console.log('Closing script...');
-//         readline.close();
-//         process.exit();
-//     },
-//     photo: function () {
-//         console.log('Affichage de la photo :', Photo);
-//         readline.prompt();
-//     }
-// };
-
+/**
+ * --------CLI--------
+ * Mise en place de l'interface en ligne de commande
+ */
 var stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding('utf8');
 
+
+/**
+ *  Ici on définit la réponse du script par rapport 
+ * à ce que va faire l'utilisateur
+ */
 stdin.on('data', function (key) {
-    if (key == '\u001B\u005B\u0043') {
-        myFiles.showNextFile()
-    }
-    else if (key == '\u001B\u005B\u0044') {
-        myFiles.showPreviousFile()
-    }
-    else if (key == '\u0003') {
-        console.log("Ending of script...")
-        process.exit();
-    }
-    else {
-        console.log(key);
-        if (key == 'e') {
-            console.log(`You actualy read ${myFiles.getFilesCountSeen()} files.`)
-        }
-        else if (key == 'c') {
-            console.log(`There are ${myFiles.getSize()} files.`)
-        }
-        else {
+
+    switch (key) {
+        //fleche de droite
+        case '\u001B\u005B\u0043':
+            listFiles.get().showNextFile();
+            break;
+        //fleche de gauche
+        case '\u001B\u005B\u0044':
+            listFiles.get().showPreviousFile();
+            break;
+        //CTRL + C
+        case '\u0003':
+            console.log("Ending of script...")
+            process.exit();
+            break;
+        case 'e':
+            console.log(`You actualy read ${listFiles.get().getFilesCountSeen()} files.`)
+            break;
+        case 'c':
+            console.log(`There are ${listFiles.get().getSize()} files.`)
+            break;
+
+        default:
             console.log('Sorry, unknown command...')
-        }
+            break;
     }
-
-
 });
 
